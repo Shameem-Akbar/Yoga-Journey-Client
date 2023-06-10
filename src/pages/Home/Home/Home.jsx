4e8db9608fd5ce23385f2f765584c93d7fb9fe71
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-06-10 22:04:28 +0600

    helmet added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home | Yoga Journey</title>
+            </Helmet>
             <h2>Home</h2>
         </div>
     );
